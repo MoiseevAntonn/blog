f2da06139a788a46cb3ffcef5cbac3c3f5661aa1
MoiseevAntonn <32195182+MoiseevAntonn@users.noreply.github.com>
2017-10-23 20:21:18 +0300

    slepok so starumi predstavliniyami

---
+++ mysite/polls/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Question,Choice
+# Register your models here.
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+
+admin.site.register(Question,QuestionAdmin)
